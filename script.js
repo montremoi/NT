48e9a51a7d4b5b3d523db8f2b5961f90d52a9ef2
nombrestriangulaires <132161012+nombrestriangulaires@users.noreply.github.com>
2023-09-22 19:08:49 +0200

    script.js

--- script.js
+++ script.js
@@ -8,14 +8,7 @@ function calculateTriangularNumber() {
     document.getElementById("triangularResult").textContent = `Le nombre triangulaire associé à ${number} est ${triangularNumber}`;
     document.getElementById("result").textContent = `Le nombre triangulaire associé à ${number} est ${triangularNumber}`;
 }
-document.addEventListener("DOMContentLoaded", function() {
-        const hiddenElement = document.querySelector(".box-1 .hidden-element");
-        const box1 = document.querySelector(".box-1");
 
-        box1.addEventListener("click", function() {
-            hiddenElement.style.display = hiddenElement.style.display === "block" ? "none" : "block";
-        });
-    });
 function toggleMenu() {
     var menu = document.getElementById("result");
     if (menu.style.display === "none" || menu.style.display === "") {
