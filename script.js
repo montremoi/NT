f61338417efc8f5ed361b507b84ba340f5ff5275
nombrestriangulaires <132161012+nombrestriangulaires@users.noreply.github.com>
2023-09-23 15:05:55 +0200

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.getElementById('showMoreButton').addEventListener('click', function() {
   var hiddenElement = document.getElementById('hiddenElement');
+  var box1 = document.querySelector('.box-1');
+
   if (hiddenElement.style.display === 'none' || hiddenElement.style.display === '') {
     hiddenElement.style.display = 'block';
+    box1.style.height = 'auto'; 
   } else {
     hiddenElement.style.display = 'none';
+    box1.style.height = 'auto';
   }
 });
 
-
-
-/* function calculateTriangularNumber() {
+function calculateTriangularNumber() {
     let input = document.getElementById("input").value;
     let number = parseInt(input);
     let triangularNumber = 0;
@@ -33,4 +35,4 @@ function showAbout() {
     var aboutSection = document.getElementById('about');
     aboutSection.style.display = 'block';
 }
-*/
+
