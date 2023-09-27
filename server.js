045ba0b5a35f6cea5a2502406e9863e4ff463c56
nombrestriangulaires <132161012+nombrestriangulaires@users.noreply.github.com>
2023-09-27 12:38:12 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
