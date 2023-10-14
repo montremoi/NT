ffff1c1ef20d06af0a057125cbe14d2bfa36d600
montremoi <146775516+montremoi@users.noreply.github.com>
2023-10-14 15:36:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -134,3 +134,9 @@ function showAbout() {
     aboutSection.style.display = 'block';
 }
 
+
+document.getElementById("send-button").onclick = function() {
+      userinput = document.getElementById("message-input").value;
+      document.getElementById("message").innerHTML = userinput;
+    }
+
